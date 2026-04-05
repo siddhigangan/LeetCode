6381589e76239a1653121a39c57cdd1d6a49bd39
siddhigangan <siddhigangan1712@gmail.com>
2026-04-05 23:08:39 +0530

    Time: 1 ms (28.32%) | Memory: 19.4 MB (24.25%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,16 +1,17 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        
-        s1_list = list(s1)
-        s2_list = list(s2)
+        return sorted(s1[0::2]) == sorted(s2[0::2]) and \
+               sorted(s1[1::2]) == sorted(s2[1::2])
+        # s1_list = list(s1)
+        # s2_list = list(s2)
  
-        if s1_list == s2_list:
-            return True
+        # if s1_list == s2_list:
+        #     return True
     
-        if s1_list[0] != s2_list[0]:
-            s1_list[0], s1_list[2] = s1_list[2], s1_list[0]
+        # if s1_list[0] != s2_list[0]:
+        #     s1_list[0], s1_list[2] = s1_list[2], s1_list[0]
             
-        if s1_list[1] != s2_list[1]:
-            s1_list[1], s1_list[3] = s1_list[3], s1_list[1]
+        # if s1_list[1] != s2_list[1]:
+        #     s1_list[1], s1_list[3] = s1_list[3], s1_list[1]
             
-        return s1_list == list(s2)
+        # return s1_list == list(s2)
